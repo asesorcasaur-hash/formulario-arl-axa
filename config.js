a63b9e93ed3c7cae11a0ddeff900472777663a93
asesorcasaur-hash <asesor.casaur@gmail.com>
2026-08-12 07:45:54 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// =====================================================================
+// CONFIGURACIÓN DE SUPABASE — Proyecto: tablero_AXA COLPATRIA
+// =====================================================================
+window.SUPABASE_URL = "https://nxwlhxgntblnllhqftvh.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_o6540FPDJND_tXNm7U9dSA_NzhCZ6kG";
